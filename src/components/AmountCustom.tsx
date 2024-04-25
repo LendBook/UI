843479219e2998af3d2879e63d6bd4d35a9c7e07
yeuyeuh <icerveramarzal@gmail.com>
2024-04-25 21:50:03 +0200

    baseline for amount custom

---
+++ src/components/AmountCustom.tsx
@@ -0,0 +1,157 @@
+import {
+  Box,
+  FormControl,
+  FormHelperText,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Paper,
+  TextField,
+} from "@mui/material";
+import React, { useEffect, useMemo, useState } from "react";
+
+//valeur à recuperer
+//price feed indiquatif in USD
+
+// Définition des types pour les props de AmountCustom
+type AmountCustomProps = {
+  title?: string;
+  tokenWalletBalance: string;
+  selectedToken?: string;
+  ratioToUSD?: number;
+  onQuantityChange?: (quantity: string) => void;
+};
+
+// Composant TableCustom
+export default function AmountCustom({
+  title = "Collateral Amount",
+  tokenWalletBalance = "0",
+  selectedToken = "USDC",
+  ratioToUSD = 1.01,
+  onQuantityChange, // Fonction de gestion du changement de quantité passée en prop
+}: AmountCustomProps) {
+  // const [tokenWalletBalance, settokenWalletBalance] = useState("15232");
+  // const [selectedToken, setSelectedToken] = useState("USDC");
+  // const ratioToUSD = 1.01;
+
+  const [label, setLabel] = useState("Enter Amount");
+
+  // ORDER VARIABLE
+  const [quantity, setQuantity] = useState("");
+
+  // Effet pour suivre les changements de quantity et appeler la fonction onQuantityChange
+  useEffect(() => {
+    if (onQuantityChange) {
+      onQuantityChange(quantity);
+    }
+  }, [quantity, onQuantityChange]);
+
+  function formatNumber(num: any) {
+    if (num >= 1000000000) {
+      return (num / 1000000000).toFixed(2) + "B";
+    } else if (num >= 1000000) {
+      return (num / 1000000).toFixed(2) + "M";
+    } else if (num >= 1000) {
+      return num.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ","); // Ajoute une virgule après les trois premiers chiffres
+    } else {
+      return num.toFixed(2);
+    }
+  }
+
+  const handleClick = () => {
+    setLabel("");
+  };
+  const handleBlur = () => {
+    if (quantity == "") {
+      setLabel("Enter Amount");
+    }
+  };
+
+  const [message, setMessage] = useState("");
+  // INPUT
+  const onBuyBudgetChange = (e: any) => {
+    try {
+      if (e.target.value === "") {
+        setQuantity("");
+        setMessage("");
+      } else {
+        let inputValue = e.target.value;
+        inputValue = inputValue.replace(/,/g, ".");
+        let amount = inputValue.replace(/[^0-9.]/g, "");
+
+        // Si plus d'un point décimal est présent, retirer tous les points sauf le premier
+        const decimalCount = amount.split(".").length - 1;
+        if (decimalCount > 1) {
+          amount = amount.replace(/(\.[^.]*)\./g, "$1");
+        }
+
+        //let amount = e.target.value;
+        //amount = amount.toString().replace(/^0+/, "");
+        if (amount.length === 0) amount = "";
+        if (amount[0] === ".") amount = "0" + amount;
+        setQuantity(amount);
+        setMessage("$" + formatNumber(parseFloat(amount) * ratioToUSD));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleMaxClick = () => {
+    const maxAmount = tokenWalletBalance;
+    setQuantity(maxAmount);
+    setMessage("$" + formatNumber(parseFloat(maxAmount) * ratioToUSD));
+    setLabel("");
+  };
+
+  return (
+    <Box className="max-w-[300px]">
+      <span className="text-gray-700 text-[24px] font-bold">{title}</span>
+      <Paper
+        elevation={1}
+        style={{ padding: "0px", width: "100%" }}
+        className="flex flex-col"
+      >
+        <TextField
+          label={label}
+          margin="none"
+          value={quantity}
+          onChange={onBuyBudgetChange}
+          onClick={handleClick}
+          onBlur={handleBlur}
+          InputLabelProps={{
+            style: { color: "#434343" },
+          }}
+          InputProps={{
+            style: {
+              color: "#333333",
+              backgroundColor: "white",
+            },
+            endAdornment: (
+              <InputAdornment position="end">{selectedToken}</InputAdornment>
+            ),
+          }}
+          sx={{
+            fieldset: { borderColor: "transparent" },
+          }}
+          variant="outlined"
+        />
+      </Paper>
+      <div className="flex justify-between items-center">
+        <span className="text-gray-700 text-[12px]">{message}</span>
+        <div className="flex items-center">
+          <span className="text-black text-[12px]">
+            Balance: {formatNumber(parseFloat(tokenWalletBalance))}
+          </span>
+          <div style={{ width: "10px" }} /> {/* Espace */}
+          <button
+            className="text-black text-[12px] underline font-bold"
+            onClick={handleMaxClick}
+          >
+            Max
+          </button>
+        </div>
+      </div>
+    </Box>
+  );
+}
