4d5ee4fcf2b4949394316d9303587f4001153a27
0xAdamdev <adam.chafqani@outlook.com>
2024-05-07 14:43:53 +0200

    update template vue

--- src/components/Template/Index.tsx
+++ src/components/Template/Index.tsx
@@ -1,7 +1,4 @@
-import Orderbook from "../Orderbook/Orderbook";
-import { OrderProvider } from "../Orderbook/OrderContext";
-import { Box, Card, CardContent, Grid, Typography } from "@mui/material";
-import TradeModule from "../Trade/TradeModule";
+import { Box, Card } from "@mui/material";
 import TableCustom from "../TableCustom";
 import AmountCustom from "../AmountCustom";
 import { useState } from "react";
@@ -9,33 +6,16 @@ import MetricCustom from "../MetricCustom";
 
 const dataTable = [
   {
-    "Buy Price": "3,400 USDC",
-    "Total Supply": "10M USDC",
-    "Net APY": "10.3%",
-    Utilization: "85%",
-    "My Supply": "",
+    "Data A": "1000 USDC",
+    "Data B": "1000 USDC",
   },
   {
-    "Buy Price": "3,200 USDC",
-    "Total Supply": "8.2M USDC",
-    "Net APY": "7.8%",
-    Utilization: "76%",
-    "My Supply": "7,000 USDC",
+    "Data A": "1000 USDC",
+    "Data B": "1000 USDC",
   },
   {
-    "Buy Price": "3,000 USDC",
-    "Total Supply": "3.3M USDC",
-    "Net APY": "6.1%",
-    Utilization: "60%",
-    "My Supply": "3,000 USDC",
-  },
-];
-
-const dataMetric = [
-  {
-    title: "My amount already supplied",
-    value: "10000",
-    unit: "USDC",
+    "Data A": "1000 USDC",
+    "Data B": "1000 USDC",
   },
 ];
 
@@ -69,10 +49,7 @@ const Index = () => {
     setBuyPrice(rowData["Buy Price"]);
     updateButtonClickable(supplyAmountQuantity, rowData["Buy Price"]);
   };
-
-  const handleButtonClick = () => {
-    setMessage("Button clicked!");
-  };
+  
 
   return (
     <div className="mt-20 ml-72 mr-4">
@@ -88,30 +65,19 @@ const Index = () => {
         <Box>
           <div>
             <div className=" text-primary text-[48px] font-bold">
-              Lend to Earn
+              TEMPLATE
             </div>
             <div className="flex flex-col md-plus:flex-row space-between items-baseline">
               <div className="container" style={{ marginBottom: "10px" }}>
-                <AmountCustom
-                  title="Supply Amount"
-                  tokenWalletBalance="11320"
-                  selectedToken="USDC"
-                  ratioToUSD={1.01}
-                  onQuantityChange={handleQuantityChange}
-                />
+                Template
               </div>
-
-              <div className="flex mt-10 md-plus:ml-10 md-plus:mt-0">
-                <div className="container">
-                  <MetricCustom data={dataMetric} />
-                </div>
-              </div>
+              
             </div>
 
             <div className="flex mt-10">
               <div className="container">
                 <TableCustom
-                  title="Select a Buy Price"
+                  title="Template"
                   data={dataTable}
                   clickableRows={true}
                   onRowClick={handleRowClick}
@@ -119,11 +85,6 @@ const Index = () => {
               </div>
             </div>
             
-            <div className="container">
-              <span className="text-success text-[12px] font-bold">
-                {message}
-              </span>
-            </div>
           </div>
         </Box>
       </Card>
